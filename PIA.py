537a593d242b042bcf9e90eeacb4c602d15022fa
David Nieto Garcia <david.nietog@uanl.edu.mx>
2025-11-02 13:08:01 -0600

    agregando opcion 3 y la opcion 4 que es la nueva para el PIA

--- PIA.py
+++ PIA.py
@@ -335,4 +335,238 @@ while True:
                 except:
                     print(f"Se produjo el siguiente error: {sys.exc_info()[0]}")
                     break
+        case 3:
+            print("\n===============Consultar reservaciones===============\n")
+
+            while True:
+                fecha_consultada = input(f"Ingrese la fecha a consultar (mm/dd/aaaa) o dejar vacio para asignar la fecha de hoy: ")
+                
+                if not fecha_consultada:
+                    fecha_consulta_dt = fecha_hoy
+                    break
+                    
+                try:
+                    fecha_consulta_dt = dt.datetime.strptime(fecha_consultada, "%m/%d/%Y").date()
+                    break
+                except ValueError:
+                    print("Favor de digitar una fecha valida\n")
+                    continue
+        
+            filas_tabla = []
+            valor = (fecha_consulta_dt,)    
+            try:
+                with sqlite3.connect("Eventos.db", detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
+                    mi_cursor = conn.cursor()
+                    consulta = """
+                        SELECT S.NOMBRE, C.NOMBRE, C.APELLIDO, E.NOMBRE_EVENTO, E.TURNO
+                        FROM EVENTOS AS E INNER JOIN SALAS AS S ON E.ID_SALA = S.ID_SALA
+                        INNER JOIN CLIENTES AS C ON E.ID_CLIENTE = C.ID_CLIENTE
+                        WHERE DATE(E.FECHA) = ? AND E.DISPONIBILIDAD = 1
+                    """   
+                    mi_cursor.execute(consulta, valor)            
+                    eventos_en_fecha = mi_cursor.fetchall()
+            except Error as e:
+                print(f"Error de base de datos al consultar eventos: {e}")
+                continue
+            except:
+                print(f"Se produjo el siguiente error: {sys.exc_info()[0]}")
+                continue
+            
+            if not eventos_en_fecha:
+                print(f"\nNo hay eventos registrados para la fecha {fecha_consulta_dt.strftime('%m/%d/%Y')}\n")
+                continue
+            else:
+                for evento in eventos_en_fecha:
+                    nombre_sala = evento[0]
+                    nombre_cliente = f"{evento[1]} {evento[2]}"
+                    nombre_evento = evento[3]
+                    turno = evento[4]
+                    
+                    filas_tabla.append([nombre_sala, nombre_cliente, nombre_evento, turno])
+                        
+                headers = ["SALA","CLIENTE","EVENTO","TURNO"]
+
+                tabla = tabulate(filas_tabla, headers, tablefmt="fancy_grid", stralign="center", numalign="center")
+
+                print("*" * 70)
+                print(f"\t** REPORTE DE RESERVACIONES PARA LA FECHA {fecha_consultada} **")
+                print("*" * 70)
+                print(tabla)
+                print("*" * 70)
+                print("\t**\t\tFIN DEL REPORTE\t\t**")    
+                print("*" * 70)
+                
+                while True:
+                    print("\n==========Menu de exportacion de datos==========")
+                    print("A. Guardar en JSON")
+                    print("B. Guardar en CSV")
+                    print("C. Guardar en Excel")
+                    opcion_exportacion = input("Ingrese una opcion o bien digite alguna otra tecla para regresar al menu principal: ")
+                    
+                    datos_a_exportar = [headers] + filas_tabla
+                    nombre_base_archivo = "reservaciones"
+                    
+                    match opcion_exportacion.upper():
+                        case "A":
+                            datos_json = {
+                                "fecha_consulta": fecha_consultada, 
+                                "reservaciones": [{"sala": fila[0],
+                                                "cliente": fila[1],
+                                                "evento": fila[2],
+                                                "turno": fila[3]
+                                } for fila in filas_tabla]
+                            }
+                            
+                            try:
+                                with open("reservaciones.json", "w") as archivo:
+                                    json.dump(datos_json, archivo, indent=4)
+                                print("Datos guardados correctamente en formato JSON")
+                            except Exception as e:
+                                print(f"Error al guardar en JSON: {e}")
+
+                        case "B":
+                            try:
+                                with open("reservaciones.csv","w", encoding="latin1", newline="") as archivo:
+                                    grabador=csv.writer(archivo)
+                                    grabador.writerows(datos_a_exportar)
+                                print("Datos guardados correctamente en formato CSV")
+                            except Exception as e:
+                                print(f"Error al guardar en CSV: {e}")
+
+                        case "C":
+                            try:
+                                libro = openpyxl.Workbook()
+                                hoja = libro.active
+                                hoja.title = "Reservaciones"
+                                negritas = Font(bold=True)
+                                borde_inferior = Border(bottom=Side(border_style="thick")) 
+                                centrado = Alignment(horizontal="center", vertical="center", wrap_text=True)
+
+                                for col_num, header in enumerate(headers, start=1):
+                                    celda = hoja.cell(row=1, column=col_num, value=header)
+                                    celda.font = negritas
+                                    celda.border = borde_inferior
+                                    celda.alignment = centrado
+                                
+                                for fila_num, fila in enumerate(filas_tabla, start=2):
+                                    for col_num, valor in enumerate(fila, start=1):
+                                        celda = hoja.cell(row=fila_num, column=col_num, value=valor)
+                                        celda.alignment = centrado
+
+                                for col in ["A","B","C","D"]:
+                                    hoja.column_dimensions[col].width = 30 
+
+                                libro.save(f"reservaciones.xlsx") 
+                                print("ARCHIVO EXCEL GENERADO")
+                                print("Datos guardados correctamente en formato Excel")
+                            except Exception as e:
+                                print(f"Error al guardar en Excel: {e}")
+
+                        case _:
+                            break
+                    break
+        
+        case 4:
+            print("\n===============Cancelar evento===============\n")
+            
+            while True:
+                fecha_inicio = input("Ingrese desde que fecha consultar los eventos (mm/dd/aaaa): ")
+                try:
+                    fecha_inicio = dt.datetime.strptime(fecha_inicio, "%m/%d/%Y").date()
+                    fecha_inicio_iso = fecha_inicio.isoformat()
+                    if (fecha_inicio - fecha_hoy).days <= 2:
+                        print(f"La cancelacion solo puede realizarse con dos dias de anticipacion\n")
+                        continue
+                    break
+                except ValueError:
+                    print("Favor de digitar una fecha valida\n")
+                    continue
+                
+
+            while True:
+                fecha_fin = input("Ingrese hasta que fecha consultar los eventos (mm/dd/aaaa): ")
+                try:
+                    fecha_fin = dt.datetime.strptime(fecha_fin, "%m/%d/%Y").date()
+                    if fecha_fin < fecha_inicio:
+                        print("La fecha final no puede ser menor a la fecha inicial\n")
+                        continue
+                    fecha_fin_iso = fecha_fin.isoformat()
+                    break
+                except ValueError:
+                    print("Favor de digitar una fecha valida\n")
+                    continue
+            
+            eventos_en_rango = []
+            folios_eventos_validos = []
+            valores = (fecha_inicio, fecha_fin)
+            
+            try:
+                with sqlite3.connect("Eventos.db", detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
+                    mi_cursor = conn.cursor()
+                    consulta = """
+                        SELECT ID_EVENTO, NOMBRE_EVENTO, TURNO, strftime('%m/%d/%Y', FECHA)
+                        FROM EVENTOS 
+                        WHERE DATE(FECHA) BETWEEN ? AND ? AND DISPONIBILIDAD = 1
+                    """   
+                    mi_cursor.execute(consulta, valores)
+                    eventos_en_rango = mi_cursor.fetchall() 
+                    folios_eventos_validos = [evento[0] for evento in eventos_en_rango]
+            except Error as e:
+                print(f"Error de base de datos al consultar eventos: {e}")
+                continue
+            except:
+                print(f"Se produjo el siguiente error: {sys.exc_info()[0]}")
+                continue
+
+            if not eventos_en_rango:
+                print(f"\nNo hay eventos registrados entre {fecha_inicio.strftime('%m/%d/%Y')} y {fecha_fin.strftime('%m/%d/%Y')}\n")
+                continue
+            else:
+                print(f"\n\t**********Eventos registrados entre {fecha_inicio.strftime('%m/%d/%Y')} y {fecha_fin.strftime('%m/%d/%Y')}**********")
+                
+                filas_tabla_eventos = []
+                for evento in eventos_en_rango:
+                    folio = evento[0]
+                    nombre_evento = evento[1]
+                    turno_evento = evento[2]
+                    fecha_evento = evento[3]
+                    
+                    filas_tabla_eventos.append([folio, nombre_evento, turno_evento, fecha_evento])
+                
+                headers = ["Folio del evento", "Nombre delEvento", "Turno", "Fecha"]
+                tabla = tabulate(filas_tabla_eventos, headers, tablefmt="fancy_grid", stralign="center", numalign="center")
+                print(tabla)
+            
+            while True:
+                try:
+                    folio_evento_elegido = int(input("Ingrese el folio del evento a cancelar: "))
+                    if folio_evento_elegido not in folios_eventos_validos:
+                        print("Elegir folio de evento dentro de las opciones mostradas\n")
+                        print(tabla)
+                        continue
+                    break
+                except ValueError:
+                    print("Favor de digitar un numero valido\n")
+                    continue
+            valor = (folio_evento_elegido,)
+            
+            while True:
+                confirmacion = input("Colocar S para confirmar la cancelacion del evento, digitar cualquier otra tecla para regresar al menu principal: ")
+                if confirmacion.upper() == "S":
+                    try:
+                        with sqlite3.connect("Eventos.db") as conn:
+                            mi_cursor = conn.cursor()
+                            mi_cursor.execute("UPDATE EVENTOS SET DISPONIBILIDAD = 0 WHERE ID_EVENTO = ?", (valor))
+                            mi_cursor.execute("PRAGMA foreign_keys = 1")
+                            print("***Evento cancelado con éxito***")
+                            break
+                    except Error as e:
+                        print(f"Error de base de datos al cancelar el evento: {e}")
+                        break
+                    except:
+                        print(f"Se produjo el siguiente error: {sys.exc_info()[0]}")
+                        break
+                else:
+                    print("Operación cancelada por el usuario")
+                break
                 
